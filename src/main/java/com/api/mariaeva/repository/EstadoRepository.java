10bab54ff9c3f13033326ae6082cc40d9d3f43d1
RafaelRamosMachado <rafa.ramosmachado@gmail.com>
2023-01-03 10:52:06 -0300

    Projeto back de um ecommerce de ternos femininos

---
+++ src/main/java/com/api/mariaeva/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.api.mariaeva.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.mariaeva.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+}
